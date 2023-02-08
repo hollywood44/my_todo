27e16b6a85a70de910fa8e497a3a94363f2a619a
jonguk <hollywood44@naver.com>
2023-02-08 15:53:12 +0900

    매일자정 완료일이 어제까지인 todo를 failed로 바꿈

--- src/main/java/com/share/my_todo/batch/BatchScheduler.java
+++ src/main/java/com/share/my_todo/batch/BatchScheduler.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -21,10 +20,11 @@ import java.util.Map;
 @EnableScheduling
 @RequiredArgsConstructor
 @Component
-public class NotificationScheduler {
+public class BatchScheduler {
 
     private final TomorrowBatchConfig tomorrowBatchConfig;
     private final FollowerDeleteBatchConfig followerDeleteBatchConfig;
+    private final ProceedingToFailedBatchConfig proceedingToFailedBatchConfig;
     private final JobLauncher jobLauncher;
 
     // 초 분 시 날짜 월 요일
@@ -42,6 +42,32 @@ public class NotificationScheduler {
             JobParameters parameters = new JobParameters(jobParametersMap);
 
             JobExecution jobExecution = jobLauncher.run(tomorrowBatchConfig.notificationJob(), parameters);
+
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (JobRestartException e) {
+            e.printStackTrace();
+        } catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        } catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "0 0 00 * * ?")
+    public void proceedingToFailedSchedule() {
+        try {
+            Map <String, JobParameter> jobParametersMap = new HashMap <> ();
+
+            SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date time = new Date();
+            String time1 = format1.format(time);
+
+            jobParametersMap.put("requestDate", new JobParameter(time1));
+
+            JobParameters parameters = new JobParameters(jobParametersMap);
+
+            JobExecution jobExecution = jobLauncher.run(proceedingToFailedBatchConfig.PtoFJob(), parameters);
 
         } catch (JobExecutionAlreadyRunningException e) {
             e.printStackTrace();
---
+++ src/main/java/com/share/my_todo/batch/ProceedingToFailedBatchConfig.java
@@ -0,0 +1,86 @@
+package com.share.my_todo.batch;
+
+import com.share.my_todo.entity.notice.Notice;
+import com.share.my_todo.entity.todo.Todo;
+import com.share.my_todo.service.NoticeService;
+import com.share.my_todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDate;
+
+@Configuration
+@RequiredArgsConstructor
+@Log4j2
+@EnableBatchProcessing
+public class ProceedingToFailedBatchConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private final TodoService todoService;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Job PtoFJob() {
+        return jobBuilderFactory.get("ProceedingToFailedJob")
+                .start(ProceedingToFailedStep())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step ProceedingToFailedStep() {
+        return stepBuilderFactory.get("ProceedingToFailedStep1")
+                .<Todo, Todo>chunk(1000)
+                .reader(PtoFReader(null))
+                .processor(PtoFProcessor(null))
+                .writer(PtoFWriter(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Todo> PtoFReader(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        String yesterdayDate = todoService.dateToString(LocalDate.now().minusDays(1));
+
+        return new JpaPagingItemReaderBuilder<Todo>()
+                .name("jpaPagingItemReaderForPtoFJob")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(1000)
+                .queryString("SELECT t FROM Todo t WHERE finish_date = " + yesterdayDate + " AND NOT Progress = 'Failed' ORDER BY todo_id ASC")
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<Todo, Todo> PtoFProcessor(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return new ItemProcessor<Todo, Todo>() {
+            @Override
+            public Todo process(Todo item) throws Exception {
+                item.progressChangeToFailed();
+                return item;
+            }
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JpaItemWriter<Todo> PtoFWriter(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return new JpaItemWriterBuilder<Todo>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+}
