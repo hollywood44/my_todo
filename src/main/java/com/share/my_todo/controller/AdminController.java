7f370fcb33c01cd0fa893b77fdbc3ed7c098b73c
jonguk <hollywood44@naver.com>
2023-02-10 04:34:43 +0900

    restcontrollr -> Controller

--- src/main/java/com/share/my_todo/controller/AdminController.java
+++ src/main/java/com/share/my_todo/controller/AdminController.java
@@ -3,9 +3,10 @@ package com.share.my_todo.controller;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
 public class AdminController {
--- src/main/java/com/share/my_todo/controller/NoticeController.java
+++ src/main/java/com/share/my_todo/controller/NoticeController.java
@@ -2,10 +2,11 @@ package com.share.my_todo.controller;
 
 import com.share.my_todo.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/notice")
 public class NoticeController {
