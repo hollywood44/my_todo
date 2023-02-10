da6d46dbbcc5b31ad87e10d38a660fb53af5ff6d
jonguk <hollywood44@naver.com>
2023-02-10 21:19:37 +0900

    친구목록 페이지

--- src/main/java/com/share/my_todo/controller/FriendController.java
+++ src/main/java/com/share/my_todo/controller/FriendController.java
@@ -1,15 +1,26 @@
 package com.share.my_todo.controller;
 
+import com.share.my_todo.entity.member.Member;
 import com.share.my_todo.service.FriendService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/friend")
+@RequestMapping("/friend")
 public class FriendController {
 
     private final FriendService friendService;
+
+    @GetMapping("/list")
+    public String friendListPage(@AuthenticationPrincipal Member member, Model model) {
+        model.addAttribute("friendList", friendService.getFriendList(member.getMemberId()));
+
+        return "friendList";
+    }
 }
